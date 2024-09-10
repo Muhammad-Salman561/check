3f12169532cc51281887d53589c236daecdbdf94
Muhammad-Salman561 <SalmanOdhejo@gmail.com>
2024-09-10 11:30:25 +0500

    add

---
+++ script.js
@@ -0,0 +1,53 @@
+let userInput = document.getElementById('usermarks');
+let submitBtn = document.getElementById("submit");
+let divEle = document.createElement("div");
+let Username = document.getElementById("name");
+
+
+// let user = Username.value;
+// console.log(user);
+
+
+ function marksheet(){
+     
+    let userVal = userInput.value
+    if (userVal >= 500) {
+        document.write("<h1>Congratulations <br />your grade is A+</h1>");
+
+    }else if(userVal >= 400){
+        document.write("<h1> Good your grade is A</h1>");
+    }
+
+    else if(userVal >= 300){
+        document.write("<h1>your grade is B </h1>");
+    }
+    else if(userVal >= 200){
+        document.write("<h1>your grade is C </h1>");
+    }
+    else {
+        document.write("<h1>Sorry you are fail</h1>");
+    }
+
+
+ }
+    
+
+
+
+
+
+
+
+
+
+
+// }
+
+// let x = 15;
+
+
+// if(x > 30){
+//     document.write("1st")
+// }else{
+//     document.write("2nd")
+// }
