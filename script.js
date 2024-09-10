b1d7576ee9b1bae6b11fb739d70e5cd9e7ce12a5
Muhammad-Salman561 <SalmanOdhejo@gmail.com>
2024-09-10 11:36:19 +0500

    add

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+// let userInput = document.getElementById('usermarks');
+// let submitBtn = document.getElementById("submit");
+// let divEle = document.createElement("div");
+// let Username = document.getElementById("name");
+
+
+// // let user = Username.value;
+// // console.log(user);
+
+
+//  function marksheet(){
+     
+//     let userVal = userInput.value
+//     if (userVal >= 500) {
+//         document.write("<h1>Congratulations <br />your grade is A+</h1>");
+
+//     }else if(userVal >= 400){
+//         document.write("<h1> Good your grade is A</h1>");
+//     }
+
+//     else if(userVal >= 300){
+//         document.write("<h1>your grade is B </h1>");
+//     }
+//     else if(userVal >= 200){
+//         document.write("<h1>your grade is C </h1>");
+//     }
+//     else {
+//         document.write("<h1>Sorry you are fail</h1>");
+//     }
+
+
+//  }
+    
+
+
+
+
+
+
+
+
+
+
+// }
+
+// let x = 15;
+
+
+// if(x > 30){
+//     document.write("1st")
+// }else{
+//     document.write("2nd")
+// }
+
+
+
 let userInput = document.getElementById('usermarks');
 let submitBtn = document.getElementById("submit");
 let divEle = document.createElement("div");
@@ -25,29 +81,19 @@ let Username = document.getElementById("name");
         document.write("<h1>your grade is C </h1>");
     }
     else {
-        document.write("<h1>Sorry you are fail</h1>");
-    }
+       document.write("<h1>Sorry you are fail</h1>");
+     }
 
 
  }
-    
+     
+
+ let x = 15;
 
 
+ if(x > 30){
+     document.write("1st")
+ }else{
+     document.write("2nd")
 
-
-
-
-
-
-
-
-// }
-
-// let x = 15;
-
-
-// if(x > 30){
-//     document.write("1st")
-// }else{
-//     document.write("2nd")
-// }
+    }
